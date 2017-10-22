937ac951f0416eda16279360dc58355d910d32eb
igor-starostenko <contactigorstar@gmail.com>
2017-10-21 17:05:17 -0700

    routes for reports

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
+   scope '/api' do
+    scope '/v1' do
+      scope '/reports' do
+        get '/' => 'reports#index'
+        post '/' => 'reports#create'
+        get '/:id' => 'reports#show'
+      end
+    end
+  end 
 end
