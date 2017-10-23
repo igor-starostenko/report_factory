a5db2c4e3fcd7fc2c3b0c4e37a78c84325b1892b
igor-starostenko <contactigorstar@gmail.com>
2017-10-22 19:55:51 -0700

    rspec reports routes tests

---
+++ app/controllers/rspec_reports_controller.rb
@@ -0,0 +1,23 @@
+class RspecReportsController < ApplicationController
+  def index
+    # render json: RspecReport.where('owner_id = ?', @project.name)
+  end
+
+  def show
+    # render json: @rspec_report
+  end
+
+  def create
+    # if @rspec_report.present?
+    #   render nothing: true, status: :conflict
+    # else
+    #   @rspec_report = RspecReport.new
+    #   @rspec_report.assign_attributes(@json['rspec_report']
+    #   if @report.save
+    #     render json: @rspec_report
+    #   else
+    #      render nothing: true, status: :bad_request
+    #   end
+    # end
+  end
+end
