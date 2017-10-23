e70637651f52d68d31cbf31a7b11f75fd4c6d662
igor-starostenko <contactigorstar@gmail.com>
2017-10-22 20:15:50 -0700

    routes spec in one file

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,8 +1,82 @@
 require 'rails_helper'
 
-RSpec.describe 'general routing', type: :routing do
-  it 'does not expose api' do
-    expect(get: '/api/v1').not_to be_routable
-    expect(get: '/api').not_to be_routable
+RSpec.describe 'routing', :routing, type: :routing do
+  context '/api/v1' do
+    it 'does not expose /api' do
+      expect(get: '/api').not_to be_routable
+    end
+
+    it 'does not expost /api/v1' do
+      expect(get: '/api/v1').not_to be_routable
+    end
+  end
+
+  context '/projects', :projects do
+    it 'routes GET /api/v1/projects to projects#index' do
+      expect(get: '/api/v1/projects').to route_to(
+        controller: 'projects',
+        action: 'index'
+      )
+    end
+
+    it 'routes POST /api/v1/projects to projects#create' do
+      expect(post: '/api/v1/projects').to route_to(
+        controller: 'projects',
+        action: 'create'
+      )
+    end
+
+    it 'routes GET /api/v1/projects/:project_name to projects#show' do
+      expect(get: '/api/v1/projects/web_project').to route_to(
+        controller: 'projects',
+        action: 'show',
+        project_name: 'web_project'
+      )
+    end
+
+    it 'routes PUT /api/v1/projects/:project_name to projects#update' do
+      expect(put: '/api/v1/projects/web_project').to route_to(
+        controller: 'projects',
+        action: 'update',
+        project_name: 'web_project'
+      )
+    end
+
+    context '/reports', :reports do
+      it 'routes GET /api/v1/projects/:project_name/reports to reports#index' do
+        expect(get: '/api/v1/projects/web_project/reports').to route_to(
+          controller: 'reports',
+          action: 'index',
+          project_name: 'web_project'
+        )
+      end
+
+      context '/rspec', :rspec_reports do
+        it 'routes GET /api/v1/projects/:project_name/reports/rspec to reports#index' do
+          expect(get: '/api/v1/projects/web_project/reports/rspec').to route_to(
+            controller: 'rspec_reports',
+            action: 'index',
+            project_name: 'web_project'
+          )
+        end
+
+        it 'routes POST /api/v1/projects/:project_name/reports/rspec to reports#create' do
+          expect(post: '/api/v1/projects/web_project/reports/rspec').to route_to(
+            controller: 'rspec_reports',
+            action: 'create',
+            project_name: 'web_project'
+          )
+        end
+
+        it 'routes GET /api/v1/projects/:project_name/reports/:id to reports#show' do
+          expect(get: '/api/v1/projects/web_project/reports/rspec/1').to route_to(
+            controller: 'rspec_reports',
+            action: 'show',
+            project_name: 'web_project',
+            id: '1'
+          )
+        end
+      end
+    end
   end
 end
