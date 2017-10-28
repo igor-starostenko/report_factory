2cfbe5aa32f67b55b67b01ad09b349b97279a3b6
igor-starostenko <contactigorstar@gmail.com>
2017-10-27 22:14:00 -0700

    fixed report relation

--- app/models/rspec_report.rb
+++ app/models/rspec_report.rb
@@ -3,7 +3,7 @@
 # DB model that represents an RSpec tests
 # report submitted for a project
 class RspecReport < ActiveRecord::Base
-  has_one :report, as: :reported
+  has_one :report, as: :reportable
   has_many :rspec_examples
   has_one :rspec_summary
 end
