1ea98d8ffd4052749caf3772a8d762204ae90a2e
igor-starostenko <contactigorstar@gmail.com>
2017-11-18 11:04:28 -0800

    cleanup user_report serializable

--- app/serializers/serializable_user_report.rb
+++ app/serializers/serializable_user_report.rb
@@ -8,25 +8,33 @@ class SerializableUserReport < JSONAPI::Serializable::Resource
   attribute :user_name { @object.user.name }
   attribute :report do
     report = @object.report
+    reportable = report.reportable
     if report.reportable_type == 'RspecReport'
-    {
-      project_name: report.project.project_name,
-      project_id: report.project.id,
-      report_id: report.id,
-      report_type: 'RSpec',
-      version: report.reportable.version,
-      examples: 
-        report.reportable.examples&.map do |example|
-          example.serializable_hash(except: :rspec_report_id).tap do |e|
-            e[:exception] = example.exception&.
-              serializable_hash(except: :rspec_example_id)
-          end
-        end,
-      summary: report.reportable.summary&.serializable_hash(except: :rspec_report_id),
-      summary_line: report.reportable.summary_line,
-      date: { created_at: report.created_at,
-              updated_at: report.updated_at }
-    }
+      {
+        project_name: report.project.project_name,
+        project_id: report.project.id,
+        report_id: report.id,
+        report_type: 'RSpec',
+        version: reportable.version,
+        examples: serialize_examples(reportable),
+        summary: serialize_summary(reportable),
+        summary_line: reportable.summary_line,
+        date: { created_at: report.created_at,
+                updated_at: report.updated_at }
+      }
     end
+  end
+
+  def serialize_examples(reportable)
+    reportable.examples&.map do |example|
+      example.serializable_hash(except: :rspec_report_id).tap do |e|
+        e[:exception] = example.exception&.
+          serializable_hash(except: :rspec_example_id)
+      end
+    end
+  end
+
+  def serialize_summary(reportable)
+    reportable.summary&.serializable_hash(except: :rspec_report_id)
   end
 end
