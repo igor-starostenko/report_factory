5b77b8ec7142e13296c51b66fb671f8b37dd9b13
igor-starostenko <contactigorstar@gmail.com>
2017-11-26 22:43:48 -0800

    rubocop cleanup cors middleware setup

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module ReportFactory
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', headers: :any, methods: %i[get post options]
       end
     end
   end
