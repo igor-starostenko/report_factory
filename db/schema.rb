df988dd5199ad75b183a00bfcc8357ed64dac6f6
igor-starostenko <contactigorstar@gmail.com>
2018-03-03 20:52:37 -0800

    added tags column to Report

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213034456) do
+ActiveRecord::Schema.define(version: 20180304025304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180213034456) do
     t.bigint "reportable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "tags", default: [], array: true
     t.index ["project_id"], name: "index_reports_on_project_id"
     t.index ["reportable_type", "reportable_id"], name: "index_reports_on_reportable_type_and_reportable_id"
   end
