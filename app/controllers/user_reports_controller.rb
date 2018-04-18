8fa074fc78906ed0aba238e1896ca89e58d6afe4
igor-starostenko <contactigorstar@gmail.com>
2018-04-18 00:02:41 -0700

    improved performance of user_reports query

--- app/controllers/user_reports_controller.rb
+++ app/controllers/user_reports_controller.rb
@@ -2,9 +2,16 @@
 
 # Provides interface for User Reports API
 class UserReportsController < BaseUsersController
-  before_action :set_user
+  before_action :set_user, :set_user_reports
 
   def index
-    render jsonapi: @user.user_reports, status: :ok
+    render jsonapi: @user_reports, status: :ok
+  end
+
+  private
+
+  def set_user_reports
+    @user_reports = UserReport.where(user_id: @user.id)
+      .includes([:user, report: [:project, { reportable: [{ examples: :exception }, :summary] }]])
   end
 end
