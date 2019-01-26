38c4db51e0d0c664b982559ad06c844ad32d1c8e
igor-starostenko <contactigorstar@gmail.com>
2019-01-26 12:28:10 -0800

    resolve type three arguments

--- app/graphql/report_factory_schema.rb
+++ app/graphql/report_factory_schema.rb
@@ -4,7 +4,7 @@ ReportFactorySchema = GraphQL::Schema.define do
   use GraphQL::Batch
   enable_preloading
 
-  resolve_type ->(record, ctx) { "#{record.class.name}Type".constantize }
+  resolve_type ->(record, _obj, _ctx) { "#{record.class.name}Type".constantize }
 
   # mutation(Types::MutationType)
   query(Types::QueryType)
